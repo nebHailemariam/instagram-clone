4614eb84cc35bc5185a7932ca19ec19d87086c6e
nebHailemariam <nebhailemariam@gmail.com>
2021-12-29 20:59:51 +0300

    refactor: move register function to registration actions file

--- client/src/_actions/authentication.actions.js
+++ client/src/_actions/authentication.actions.js
@@ -4,7 +4,6 @@ import { userConstants } from "../_constants/user.constants";
 export const authenticationActions = {
   login,
   logout,
-  register,
 };
 
 const login = (email, password) => {
@@ -34,26 +33,5 @@ const logout = () => {
   };
   logout = () => {
     return { type: userConstants.LOGOUT };
-  };
-};
-
-const register = (fullName, email, password) => {
-  return async (dispatch) => {
-    dispatch(request());
-    try {
-      await userService.register(fullName, email, password);
-      dispatch(success());
-    } catch (error) {
-      dispatch(failure(error));
-    }
-  };
-  request = () => {
-    return { type: userConstants.REGISTRATION_REQUEST };
-  };
-  success = () => {
-    return { type: userConstants.REGISTRATION_SUCCESS };
-  };
-  failure = (error) => {
-    return { type: userConstants.REGISTRATION_FAILURE, payload: { error } };
   };
 };
