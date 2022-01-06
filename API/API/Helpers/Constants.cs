049628cec44d13e547b3bc6b2b2cdf4f8a63d43d
nebHailemariam <nebhailemariam@gmail.com>
2022-01-06 10:32:52 +0300

    refactor: rename API folder to server

--- API/API/Helpers/Constants.cs
+++
@@ -1,7 +0,0 @@
-namespace API.Helpers
-{
-    public static class Constants
-    {
-        public const int DB_FAILURE_MAX_RETRY = 3;
-    }
-}
--- API/API/Helpers/PolicyConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace API.Helpers
-{
-    public static class PolicyConstants
-    {
-        public const string IS_USER = "IsUser";
-        public const string IS_ADMIN = "IsAdmin";
-        public const string IS_SUPERADMIN = "IsSuperAdmin";
-    }
-}
