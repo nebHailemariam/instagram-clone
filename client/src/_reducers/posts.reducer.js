08f5573534fe4c7d030073ff1e8c56debf183092
nebHailemariam <nebhailemariam@gmail.com>
2022-01-12 23:52:53 +0300

    feat: add signalr client

--- client/src/_reducers/posts.reducer.js
+++ client/src/_reducers/posts.reducer.js
@@ -1,4 +1,4 @@
-import { postConstants } from "../_constants/post.constats";
+import { postConstants } from "../_constants/post.constants";
 
 const initialPostsState = { posts: [] };
 
@@ -9,6 +9,16 @@ const posts = (state = initialPostsState, action) => {
     return { ...state, loading: false, posts: action.payload.posts };
   } else if (action.type === postConstants.GET_POSTS_FAILURE) {
     return { ...state, loading: false, error: action.payload.error };
+  } else if (action.type === postConstants.ADD_POST_REQUEST) {
+    return { ...state, loadingOnePost: true };
+  } else if (action.type === postConstants.ADD_POST_SUCCESS) {
+    return {
+      ...state,
+      loadingOnePost: false,
+      posts: [action.payload.post, ...state.posts],
+    };
+  } else if (action.type === postConstants.ADD_POST_FAILURE) {
+    return { ...state, loadingOnePost: false, error: action.payload.error };
   } else return state;
 };
 
--- client/src/pages/Home/HomePage.js
+++ client/src/pages/Home/HomePage.js
@@ -2,15 +2,35 @@ import { useLayoutEffect } from "react";
 import { connect, useDispatch, useSelector } from "react-redux";
 import { postActions } from "../../_actions/post.actions";
 import Post from "../../_components/Post/Post";
+import { HubConnectionBuilder, HttpTransportType } from "@microsoft/signalr";
+import config from "../../_helpers/config";
 
 const Home = (props) => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts.posts);
 
   useLayoutEffect(() => {
+    const connection = new HubConnectionBuilder()
+      .withUrl(config.apiUrl + "/postHub", {
+        skipNegotiation: true,
+        transport: HttpTransportType.WebSockets,
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    connection
+      .start()
+      .then((result) => {
+        connection.on("ReceiveNewPost", (newPost) => {
+          dispatch(postActions.addPost(newPost));
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
+
     dispatch(postActions.getPosts());
   }, [dispatch]);
-  console.log(posts);
 
   const listPosts = posts.map((post) => {
     return (
